0811473301135db1b00f90ad223b935d45e9c901
urigross <urigross@gmail.com>
2019-12-31 00:26:01 +0200

    changed on menu background according to routing

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+    private route: string;
+  constructor(location: Location, private router: Router) {
+      router.events.subscribe( val => {
+          if (location.path() !== '') {
+              this.route = location.path();
+              console.log(this.route);
 
-  constructor() { }
+      } else {
+          this.route = 'home';
+          console.log(this.route);
+
+      }
+    });
+   }
 
   ngOnInit() {
+
   }
 
+  private  getbackgroundColor(): string {
+      return this.route === '/heb/about' ?  'black' :  '';
+  }
 }
+
