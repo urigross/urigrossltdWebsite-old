ce12f794d2589dd0fcdf370cca5e6e4fed93a644
urigross <urigross@gmail.com>
2020-02-19 12:12:04 +0200

    Added text to mdf pages. Alinged text

--- src/app/components/heb/mdf/mdf/mdf.component.ts
+++ src/app/components/heb/mdf/mdf/mdf.component.ts
@@ -10,7 +10,6 @@ export class MdfComponent implements OnInit {
   @Output() stringOutput = new EventEmitter<string>();
 
   sendChoiceToParent(value:string){
-    console.log(value);
     this.stringOutput.emit(value);
   }
 
