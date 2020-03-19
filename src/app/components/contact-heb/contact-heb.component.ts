f045396fe2a90e64996da8f5d0ddab6720990231
urigross <urigross@gmail.com>
2020-03-20 01:06:15 +0200

    Added contact message element with regex validation and error messages. contact message and email needs css positioning

--- src/app/components/contact-heb/contact-heb.component.ts
+++ src/app/components/contact-heb/contact-heb.component.ts
@@ -10,10 +10,11 @@ import { format } from 'url';
 })
 export class ContactHebComponent {
   // email = new FormControl('', [Validators.required, Validators.email]);
-  firstName = new FormControl('', [Validators.required,Validators.pattern("^[a-z\u0590-\u05fe]{2,20}")]);
-  lastName = new FormControl('', [Validators.required,Validators.pattern("^[a-z\u0590-\u05fe]{2,20}")]);
+  firstName = new FormControl('', [Validators.pattern("^[a-zA-Z\u0590-\u05fe]{2,20}")]);
+  lastName = new FormControl('', [Validators.pattern("^[a-zA-Z\u0590-\u05fe]{2,20}")]);
   // lastName = new FormControl('', [Validators.required]);
-  telephone = new FormControl('', [Validators.pattern("[0-9 ]{7}"), Validators.required]);
+  telephone = new FormControl('', [Validators.pattern("[0-9 ]{7}")]);
+  contactMessage = new FormControl('', [Validators.pattern("^[a-zA-Z\u0590-\u05fe]{2,100}")]);
 
   getFirstNameErrorMessage() {
     if (this.firstName.hasError('pattern')){
@@ -28,27 +29,16 @@ export class ContactHebComponent {
       }
     
 
-  // getLastNameErrorMessage() {
-
-  //   if (this.lastName.hasError('required')) {
-  //     return 'חובה לרשום שם משפחה';
-  //   }
-  // }
-
-  // getEmailErrorMessage() {
-  //   if (this.email.hasError('required')) {
-  //     return 'חובה לרשום כתובת מייל';
-  //   }
-
-  //   return this.email.hasError('email') ? 'כתובת המייל שהוזנה אינה תקינה' : '';
-  // }
-
   getTelephoneErrorMessage() {
-    if (this.telephone.hasError('pattern') || this.telephone.hasError('minlength')) { // pay attention: lowe case because html 
+    if (this.telephone.hasError('pattern')) { // pay attention: lowe case because html 
       return 'נא הזן טלפון תקין בן 7 ספרות';
     }
 
   }
-
+  getContactMessageErrorMessage(){
+    if (this.contactMessage.hasError('pattern')) {
+      return ('בשדה ההודעה מותרים אותיות, ספרות, פסיקים ונקודות בלבד')
+    }
+  }
   
 }
