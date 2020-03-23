66c94f1953bae18fb9fc054c64a40c9e0aae554b
urigross <urigross@gmail.com>
2020-03-23 13:18:30 +0200

    Added waze map with a link

--- src/app/components/contact-heb/contact-heb.component.ts
+++ src/app/components/contact-heb/contact-heb.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, Validators, NgForm} from '@angular/forms';
-import { format } from 'url';
 
 
 @Component({
@@ -9,13 +8,11 @@ import { format } from 'url';
   styleUrls: ['./contact-heb.component.css']
 })
 export class ContactHebComponent {
-  // email = new FormControl('', [Validators.required, Validators.email]);
   firstName = new FormControl('', [Validators.pattern("^[a-zA-Z\u0590-\u05fe]{2,20}")]);
   lastName = new FormControl('', [Validators.pattern("^[a-zA-Z\u0590-\u05fe]{2,20}")]);
-  // lastName = new FormControl('', [Validators.required]);
   telephone = new FormControl('', [Validators.pattern("[0-9\+\-]{9,13}")]);
   contactMessage = new FormControl('', [Validators.pattern("^[a-zA-Z0-9\u0590-\u05fe \.,]{2,100}")]);
-
+  
   getFirstNameErrorMessage() {
     if (this.firstName.hasError('pattern')){
       return 'פורמט שם אינו חוקי';
